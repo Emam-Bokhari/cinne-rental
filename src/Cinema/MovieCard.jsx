1e2b835d40aa0a2529ca95b82ab18f0348b13e20
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-01-25 20:11:15 +0600

    Add Rating component in MovieCard component

--- src/Cinema/MovieCard.jsx
+++ src/Cinema/MovieCard.jsx
@@ -2,21 +2,21 @@ import { Fragment } from "react"
 import starIcon from "./../assets/star.svg"
 import tagIcon from "./../assets/tag.svg"
 import { getImageUrl } from "./../utils/cine-utility"
+import Rating from "./Rating"
 
-const MovieCard = ({movie}) => {
+const MovieCard = ({ movie }) => {
     return (
         <Fragment>
-            <figure  className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
+            <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
                 <img className="w-full object-cover" src={getImageUrl(`${movie.cover}`)} alt="" />
                 <figcaption className="pt-4">
                     <h3 className="text-xl mb-1">{movie.title}</h3>
                     <p className="text-[#575A6E] text-sm mb-2">{movie.description}</p>
                     <div className="flex items-center space-x-1 mb-5">
-                        <img src={starIcon} width="14" height="14" alt="" />
-                        <img src={starIcon} width="14" height="14" alt="" />
-                        <img src={starIcon} width="14" height="14" alt="" />
-                        <img src={starIcon} width="14" height="14" alt="" />
-                        <img src={starIcon} width="14" height="14" alt="" />
+
+                        {/* rating */}
+                        <Rating rating={movie.rating} />
+                        
                     </div>
                     <a className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
                         href="#">
---
+++ src/Cinema/Rating.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react"
+import starIcon from "../assets/star.svg"
+
+const Rating = ({ rating }) => {
+    const stars = Array(rating).fill(starIcon)
+    console.log(stars)
+    return (
+        <Fragment>
+            {stars.map((star) => (
+                <img src={star} height="14" width="14" alt="star" />
+            ))}
+        </Fragment>
+    )
+}
+export default Rating
